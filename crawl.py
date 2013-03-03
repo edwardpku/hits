043d8b8bb582258549e68bb2fb17ea629d40236a
Matt Garrett <matthew.e.garrett@gmail.com>
2013-03-02 22:52:43 -0800

    Working web crawler, could be optimized

--- crawl.py
+++ crawl.py
@@ -2,35 +2,54 @@
 #matt garrett
 #cse 417
 
-#This file contains code to compare signatures stored in a .csv file
-#Signatures need to be in a very special format, which I won't describe
-#here because this code will never be used again.  Sadface.jpg
 import sys
 import urllib2
 from bs4 import BeautifulSoup
 import re
 
-#returns the base angle difference between a and b
-# returns 0 if they point the same way
-# returns 1 if they differ by pi/4
-# returns 2 if they differ by pi/2
-# returns 4 if they differ by more than pi/2
+wikiLink = re.compile("^/wiki/[^:#]*$")
 
-def explore(url, depth, stop, documents):
-    if (depth <= stop):
+def explore(url, depth, stop, documents, outbound, problems):
+    if (url not in documents and depth < stop):
+        print("adding = " + url)
+        documents.append(url)
+        outbound.append([])
         fullUrl = "http://en.wikipedia.org" + url
         request = urllib2.Request(fullUrl, headers={"User-Agent" : "Superman"})
-        socket  = urllib2.urlopen(request)
-        page    = socket.read()
-        socket.close()
-        page    = re.sub('id="References.*', '', page)
-        soup    = BeautifulSoup(page)
-        article = soup.find("div", {"id" : "mw-content-text"})
-        for link in article.find_all("a", {"href" : re.compile("/wiki/.*")}):
-            documents.add(link.get("href"))
-            explore(link.get("href"), depth + 1, stop, documents)
-    else:
-        documents.add(url)
+        try:
+            socket  = urllib2.urlopen(request)
+            page    = socket.read()
+            socket.close()
+            page    = re.sub('id="References.*', '', page)
+            soup    = BeautifulSoup(page)
+            article = soup.find("div", {"id" : "mw-content-text"})
+            index = documents.index(url)
+            if article:
+                for link in article.find_all("a", {"href" : wikiLink}):
+                    if (link.get("href") not in documents):
+                        print("exploring: " + link.get("href"))
+                        explore(link.get("href"), depth + 1, stop, documents, outbound, problems)
+                        outbound[index].append(link.get("href"))
+            else:
+                documents.pop()
+                outbound.pop()
+                problems.append(url)
+        except IOError, (errno):
+            print "HUGE ERROR MOVING ON"
+            documents.pop()
+            outbound.pop()
+
+def dumpLists(documents, outbound):
+    f = open("wikidump.txt", "w")
+    for index, url in enumerate(documents):
+        f.write(url + ":\n")
+        for link in outbound[index]:
+            f.write("   " + link + "\n")
+
+def dumpProblems(problems):
+    print("Problems")
+    for url in problems:
+        print("   " + url)
 
 #yeah I'm used to java, learning python is hard
 def main():
@@ -39,8 +58,10 @@ def main():
     else:
         url = sys.argv[1]
         stop = int(sys.argv[2])
-        documents = set()
-        explore(url, 0, stop, documents)
-    for doc in documents:
-        print doc
+        documents = []
+        outbound = []
+        problems = []
+        explore(url, 0, stop, documents, outbound, problems)
+        dumpLists(documents, outbound)
+        dumpProblems(problems)
 main()
